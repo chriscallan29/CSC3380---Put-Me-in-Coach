0e47b258af346f55bb62552985833e9111f1afff
thetimrine <trine1@lsu.edu>
2017-04-21 18:11:28 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -3,33 +3,4 @@ UserAccounts = new Mongo.Collection('users');
 if(Meteor.isClient){
     console.log("Hello client");
 
-    Router.configure({
-        layoutTemplate: 'main',
-    });
-    Router.route('/', function () {
-        this.render('Home');
-    });
-
-    Router.route('/CreateMatch', function () {
-        this.render('CreateMatch');
-
-    });
-
-    Router.route('/FindMatch', function () {
-        this.render('FindMatch');
-    });
-
-    Router.route('/ViewProfile', function () {
-        this.render('ViewProfile');
-    });
-
-    Router.route('/NewsFeed', function () {
-        this.render('NewsFeed');
-    });
-
-
-    }
-
-
-
-
+}
