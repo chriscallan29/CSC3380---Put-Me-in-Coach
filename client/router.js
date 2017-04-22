888fa30c332945c7d87b2c21d61f28e3e31ca653
thetimrine <trine1@lsu.edu>
2017-04-21 22:23:19 -0500

    Update router.js

--- client/router.js
+++ client/router.js
@@ -29,6 +29,37 @@ Router.route('/profile', function () {
     this.render('profile');
 });
 
+Router.route('/creatematch/baseball', function () {
+    this.layout('main');
+    this.render('baseball');
+
+});
+
+Router.route('/creatematch/football', function () {
+    this.layout('main');
+    this.render('football');
+});
+
+Router.route('/creatematch/gym', function () {
+    this.layout('main');
+    this.render('gym');
+});
+
+Router.route('/creatematch/running', function () {
+    this.layout('main');
+    this.render('running');
+});
+
+Router.route('/creatematch/soccer', function () {
+    this.layout('main');
+    this.render('soccer');
+});
+
+Router.route('/creatematch/tennis', function () {
+    this.layout('main');
+    this.render('tennis');
+});
+
 Router.route('/creatematch/basketball', function () {
     this.layout('main');
     this.render('basketball');
